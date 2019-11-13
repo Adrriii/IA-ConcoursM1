5f6ada64b6085837019135c560b297f5a5f7731d
Adri- <adrien.boitelle@gmail.com>
2019-11-13 13:36:41 +0100

    add player template

--- players/template.py
+++ players/template.py
@@ -2,36 +2,32 @@
 
 import time
 from Reversi import Board
+from random import randint
 from playerInterface import *
 
-class human(PlayerInterface):
+# used to easily create a new player
+class template(PlayerInterface):
 
     def __init__(self):
         self._board = Board(10)
         self._mycolor = None
 
     def getPlayerName(self):
-        return "Human Player"
+        return "Name"
 
     def getPlayerMove(self):
         if self._board.is_game_over():
             return (-1,-1)
-        moves = {}
+        
+        move = None
+
         for m in self._board.legal_moves():
-            key = str(m[1])+","+str(m[2])
-            moves[key] = m
-            print(key)
-        print(self._board)
-        correct = False
-        while not correct:
-            try:
-                m = moves[input("Next move 'x,y' : ")]
-                correct = True
-            except:
-                pass
+            # moves
+            move = m
 
         self._board.push(m)
-        return (m[1],m[2]) 
+        (c,x,y) = move
+        return (x,y) 
 
     def playOpponentMove(self, x,y):
         self._board.push([self._opponent, x, y])
